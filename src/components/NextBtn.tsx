49f7c201c151034c91222a7ee76a216700cd98c0
Vicente Jorquera <jorquerav07@gmail.com>
2024-02-12 16:08:37 -0300

    fix/minor fixes to nextbtn

--- src/components/NextBtn.tsx
+++ src/components/NextBtn.tsx
@@ -9,7 +9,7 @@ const NextBtn = ({ onSubmit }: NextBtnProps) => {
     <>
       {
         onSubmit ? (
-          <button onClick={onSubmit} type='button' className='bg-blue-900 text-white hover:bg-blue-600 transition-all duration-300 ease-in-out font-semibold rounded-lg py-3 px-8'>Next Step</button>
+          <button onClick={onSubmit} type='button' className='bg-blue-900 text-white hover:bg-blue-600 transition-all duration-300 ease-in-out font-semibold rounded-lg py-3 px-8'>{currentStep.value === 4 ? 'Confirm' : 'Next Step'}</button>
         ) : (
           <button type='submit' className='bg-blue-900 text-white hover:bg-blue-600 transition-all duration-300 ease-in-out font-semibold rounded-lg py-3 px-8'>{currentStep.value === 4 ? 'Confirm' : 'Next Step'}</button>
         )
