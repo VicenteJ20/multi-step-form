1171dc56d45f3d251b38551ae9cb4b0691d481ef
Vicente Jorquera <jorquerav07@gmail.com>
2024-02-12 15:52:14 -0300

    fix/se corrige funcionamiento de la pantalla de agradecimiento para que funcione como se pide en los requerimientos

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import SelectPlanPage from './pages/SelectPlan';
 import AddOnsPage from './pages/AddOns';
 import SummaryPage from './pages/Summary'
 import YourInfo from './pages/YourInfo';
-import FinishPage from './pages/Finish';
 
 const App = () => {
   return (
@@ -14,7 +13,6 @@ const App = () => {
         <Route path='/select-plan' element={<SelectPlanPage />} />
         <Route path='/add-ons' element={<AddOnsPage />} />
         <Route path='/summary' element={<SummaryPage />} />
-        <Route path='/finish' element={<FinishPage />} />
       </Routes>
     </>
   )
--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -1,28 +1,42 @@
-import { useNavigate } from "react-router-dom"
 import { BackBtn } from "../components/BackBtn"
 import { ButtonsFooter } from "../components/ButtonsFooter"
 import { Header } from "../components/Header"
 import { LayoutForm } from "../components/Layout"
 import { NextBtn } from "../components/NextBtn"
 import { Summary } from "../components/Step4/Summary"
-import { Step4 } from "../constants/StepsInfo"
+import { Step4, Step5Info } from "../constants/StepsInfo"
+import { useState } from "react"
 
 const SummaryPage = () => {
-  const navigate = useNavigate()
+  const [finish, setFinish] = useState(false)
 
   const handleClick = () => {
-    return navigate('/finish')
+    setFinish(true)
   }
 
   return (
     <LayoutForm>
-      <Header title={Step4.title} subtitle={Step4.description} />
-      <Summary />
-      <ButtonsFooter>
-        <BackBtn />
-        <NextBtn onSubmit={handleClick} />
-      </ButtonsFooter>
-    </LayoutForm>
+      {
+        finish ? (
+          <div className='flex flex-col gap-4 text-center h-full items-center justify-center'>
+            <img src={Step5Info.icon} alt={Step5Info.title} className='w-24 mx-auto' />
+            <Header title={Step5Info.title} subtitle={Step5Info.description} />
+          </div>
+        ) : (
+          <div className='flex flex-col gap-4 justify-between h-full'>
+            <section className='flex flex-col gap-12'>
+              <Header title={Step4.title} subtitle={Step4.description} />
+              <Summary />
+            </section>
+            <ButtonsFooter>
+              <BackBtn />
+              <NextBtn onSubmit={handleClick} />
+            </ButtonsFooter>
+          </div>
+        )
+      }
+
+    </LayoutForm >
   )
 }
 
