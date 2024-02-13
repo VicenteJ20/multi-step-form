b3d0b56b91c818fb0cf02c8da70af78686d1e798
Vicente Jorquera <jorquerav07@gmail.com>
2024-02-13 16:10:53 -0300

    fix/se corrige visibilidad del header en pantallas pequeñas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import { HeaderProps } from "../interfaces/Header";
 
 const Header = ({title, subtitle}: HeaderProps) => {
   return (
-    <header className='flex flex-col gap-3'>
+    <header className='flex flex-col gap-1.5 lg:gap-3'>
       <h1 className=' text-2xl lg:text-3xl font-bold text-blue-950'>{title}</h1>
-      <p className='text-[#a2a3a9] text-sm lg:text-xl break-words'>{subtitle}</p>
+      <p className='text-[#a2a3a9] text-base lg:text-xl break-words'>{subtitle}</p>
     </header>
   );
 }
